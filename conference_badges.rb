78bace4819f76f5b75e11abda669f632400cb0a3
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 14:43:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,17 @@
 def badge_maker
+  return phrase = "Hello, my name is #{name}."
   
 end
 
 
-def batch_badge_creator 
-  
+def batch_badge_creator(names) 
+  names = []
+  return badge_messages 
 end 
 
 
 def assign_rooms
+  room_assignments = 
   
 end 
 
