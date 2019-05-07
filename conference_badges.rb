f82235b74595d4174646294e679956d332492040
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-07 15:22:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,25 +1,42 @@
-def badge_maker
-  return phrase = "Hello, my name is #{name}."
-  
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
 
 
-def batch_badge_creator(names) 
-  names = []
-  return badge_messages 
-end 
 
-
-def assign_rooms
-  rooms = (1..7).to_a 
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
   
-end 
-
-
-def printer 
-  
+  return nuarray
 end
+
+# batch_badge_creator(people)
+
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
+
+# assign_rooms(people)
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
   
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
   
   
   
